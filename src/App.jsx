0b521b38d17a9ff9485823a49e60668bdca9b71b
Kenzhie16 <kelaco16@gmail.com>
2023-02-07 08:44:45 +0800

    adding quiz app

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+
+export default function App() {
+	const questions = [
+		{
+      questionText : 'Who lives in a pineapple under the sea?',
+      answerOptions: [
+        {answerText: 'Patrick', isCorrect: false},
+        {answerText: 'Mr Krabs', isCorrect: false},
+        {answerText: 'SpongeBob Square Pants', isCorrect: true},
+        {answerText: 'Squidward', isCorrect: false},
+      ],
+    },
+    {
+      questionText: 'The iPhone was created by which company?',
+			answerOptions: [
+				{ answerText: 'Apple', isCorrect: true },
+				{ answerText: 'Intel', isCorrect: false },
+				{ answerText: 'Amazon', isCorrect: false },
+				{ answerText: 'Microsoft', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'How many Harry Potter books are there?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+      ],
+    },
+    {
+      questionText : 'What is the scientific name of lion?',
+      answerOptions: [
+        {answerText: 'Panthera leo', isCorrect: true},
+        {answerText: 'Panthera tigris', isCorrect: false},
+        {answerText: 'Panthera pardus', isCorrect: false},
+        {answerText: 'Acinonyx jubatus', isCorrect: false},
+      ],
+    },
+    {
+      questionText : 'Who is declared as the most handsome man in the world?',
+      answerOptions: [
+        {answerText: 'Jaime Capitan', isCorrect: false},
+        {answerText: 'Kenn Elaco', isCorrect: true},
+        {answerText: 'James Dhenvir', isCorrect: false},
+        {answerText: 'Glenn Andaleon', isCorrect: false},
+      ],
+    },
+	];
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+		<div className='app'>
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
+}
